6944982575187c413a61d0aecb50fc5b641ad346
Hashmi789-source <www.hashminethmindi1@gmail.com>
2026-09-07 23:08:27 +0530

    Initial project setup

---
+++ src/test_setup.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+print("Project setup successful!")
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+print("Scikit-learn:", sklearn.__version__)
